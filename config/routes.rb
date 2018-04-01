b73077e7f42ddf10381f0784fc334bd2f0dfdee2
JosephA91 <josephnamato@gmail.com>
2018-04-01 14:40:51 +0800

    User signup working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users, controllers { registrations: 'registrations' }
+  devise_for :users, controllers: { registrations: 'registrations' }
 
 end
